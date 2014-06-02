c4655c47268d980a60cbaba8ab4bf83cb11627a1
Dody Suria Wijaya <dodysw@gmail.com>
2014-06-02 16:07:10 +0700

    ignore some files

---
+++ init.sh
@@ -0,0 +1,4 @@
+wget http://thinkstats.com/survey.py
+wget http://greenteapress.com/thinkstats/2002FemPreg.dat.gz
+wget http://greenteapress.com/thinkstats/2002FemResp.dat.gz
+wget http://greenteapress.com/thinkstats/2002Male.dat.gz
